b2c7fedb18af203922d50e418fcf1ddfafe79a75
Jirat Chokdeeanan <jirat.cho@gmail.com>
2022-07-16 14:09:43 +0700

    Support redis cluster with Cluster Topology Refresh and Replica Preferred

--- src/main/java/com/example/lettuce/configuration/RedisConfiguration.java
+++ src/main/java/com/example/lettuce/configuration/RedisConfiguration.java
@@ -1,20 +1,71 @@
 package com.example.lettuce.configuration;
 
+import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class RedisConfiguration {
 
-    @Bean
-    LettuceConnectionFactory lettuceConnectionFactory(){
-        return new LettuceConnectionFactory();
+    private final RedisProperties redisProperties;
+
+    public RedisConfiguration(RedisProperties redisProperties) {
+        this.redisProperties = redisProperties;
     }
+
     @Bean
-    RedisTemplate redisTemplate(){
-        RedisTemplate redisTemplate = new RedisTemplate();
+    LettuceConnectionFactory lettuceConnectionFactory() {
+        List<String> clusterNodes = redisProperties.getCluster().getNodes();
+        Set<RedisNode> nodes = new HashSet<RedisNode>();
+        clusterNodes.forEach(address -> nodes.add(new RedisNode(address.split(":")[0].trim(),
+                Integer.parseInt(address.split(":")[1]))));
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.setClusterNodes(nodes);
+        clusterConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        clusterConfiguration.setMaxRedirects(redisProperties.getCluster().getMaxRedirects());
+
+        GenericObjectPoolConfig<RedisProperties.Lettuce> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxIdle(redisProperties.getLettuce().getPool().getMaxIdle());
+        poolConfig.setMinIdle(redisProperties.getLettuce().getPool().getMinIdle());
+        poolConfig.setMaxTotal(redisProperties.getLettuce().getPool().getMaxActive());
+
+        return new LettuceConnectionFactory(clusterConfiguration,
+                getLettuceClientConfiguration(poolConfig));
+    }
+
+    private LettuceClientConfiguration getLettuceClientConfiguration(GenericObjectPoolConfig<RedisProperties.Lettuce> genericObjectPoolConfig) {
+        ClusterTopologyRefreshOptions topologyRefreshOptions =
+                ClusterTopologyRefreshOptions.builder()
+                        .enableAllAdaptiveRefreshTriggers()
+                        .adaptiveRefreshTriggersTimeout(Duration.ofSeconds(15))
+                        .enablePeriodicRefresh(Duration.ofSeconds(15))
+                        .build();
+        return LettucePoolingClientConfiguration.builder()
+                .poolConfig(genericObjectPoolConfig)
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .clientOptions(ClusterClientOptions.builder().topologyRefreshOptions(topologyRefreshOptions).build())
+                .build();
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate(){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         return redisTemplate;
     }
